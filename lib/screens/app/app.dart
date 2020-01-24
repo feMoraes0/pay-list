789293650ccedb8ae689316db069a8b5eb092068
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-01-24 19:35:08 -0300

    Refactored pay list to new file component

--- lib/screens/app/app.dart
+++ lib/screens/app/app.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:flutter/rendering.dart';
 import 'package:pay_list/components/credit_card.dart';
+import 'package:pay_list/components/pay_list.dart';
 
 class AppScreen extends StatefulWidget {
   @override
@@ -8,36 +9,7 @@ class AppScreen extends StatefulWidget {
 }
 
 class _AppScreenState extends State<AppScreen> {
-  final List<String> itens = [
-    "teste 01",
-    "teste 02",
-    "teste 03",
-    "teste 04",
-    "teste 05",
-    "teste 06",
-    "teste 07",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-    "teste 08",
-  ];
-  double containerHeight = 435.0;
-  double limitValue = 580.0;
-  double minValue = 435.0;
-  ScrollPhysics _physics = AlwaysScrollableScrollPhysics();
-  ScrollController _scrollController = ScrollController();
-  GlobalKey listKey = new GlobalKey();
-
+  
   Widget buildCardOption(IconData icon, String text, ThemeData theme) {
     return Column(
       children: <Widget>[
@@ -132,59 +104,7 @@ class _AppScreenState extends State<AppScreen> {
             ),
             Positioned(
               bottom: 0,
-              child: AnimatedContainer(
-                duration: Duration(seconds: 1),
-                curve: Curves.fastOutSlowIn,
-                width: size.width,
-                height: this.containerHeight,
-                decoration: BoxDecoration(
-                  color: theme.backgroundColor,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(0.0, -5.0),
-                      blurRadius: 2.0,
-                    ),
-                  ],
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(8.0),
-                    topRight: Radius.circular(8.0),
-                  ),
-                ),
-                child: NotificationListener<ScrollNotification>(
-                  onNotification: (scrollNotification) {
-                    ScrollDirection direction =
-                        this._scrollController.position.userScrollDirection;
-                    double scroll = scrollNotification.metrics.pixels;
-                    if (direction == ScrollDirection.reverse) {
-                      double futurePosition = scroll + this.containerHeight;
-                      if (futurePosition < this.limitValue) {
-                        setState(() {
-                          this.containerHeight = this.limitValue;
-                        });
-                      }
-                    } else if (direction == ScrollDirection.forward &&
-                        scroll == 0.0) {
-                      setState(() {
-                        this.containerHeight = this.minValue;
-                      });
-                    }
-                    return true;
-                  },
-                  child: ListView.builder(
-                    key: this.listKey,
-                    controller: this._scrollController,
-                    physics: this._physics,
-                    itemCount: this.itens.length,
-                    itemBuilder: (context, position) {
-                      return Container(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(this.itens[position]),
-                      );
-                    },
-                  ),
-                ),
-              ),
+              child: PayList(),
             ),
           ],
         ),
