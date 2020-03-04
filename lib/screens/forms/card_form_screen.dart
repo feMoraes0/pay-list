12d0ee78209fd63dff54e837c01decc2f5181018
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-03-04 17:08:33 +0100

    refactor<style>: card form screen

--- lib/screens/forms/card_form_screen.dart
+++ lib/screens/forms/card_form_screen.dart
@@ -4,52 +4,20 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:pay_list/components/button_component.dart';
 import 'package:pay_list/components/input_component.dart';
 
-class Item {
-  const Item(this.name, this.icon);
-  final String name;
-  final Icon icon;
-}
-
 class CardFormScreen extends StatefulWidget {
   @override
   _CardFormScreenState createState() => _CardFormScreenState();
 }
 
 class _CardFormScreenState extends State<CardFormScreen> {
-  TextEditingController _emailController = new TextEditingController();
-  
-  Item selectedUser;
-  List<Item> users = <Item>[
-    const Item(
-        'Android',
-        Icon(
-          Icons.android,
-          color: const Color(0xFF167F67),
-        )),
-    const Item(
-        'Flutter',
-        Icon(
-          Icons.flag,
-          color: const Color(0xFF167F67),
-        )),
-    const Item(
-        'ReactNative',
-        Icon(
-          Icons.format_indent_decrease,
-          color: const Color(0xFF167F67),
-        )),
-    const Item(
-        'iOS',
-        Icon(
-          Icons.mobile_screen_share,
-          color: const Color(0xFF167F67),
-        )),
-  ];
+  TextEditingController _titleController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    ThemeData theme = Theme.of(context);
     return Scaffold(
+      backgroundColor: theme.backgroundColor,
       appBar: AppBar(
         leading: GestureDetector(
           onTap: () {
@@ -61,39 +29,30 @@ class _CardFormScreenState extends State<CardFormScreen> {
             color: Colors.black,
           ),
         ),
-        elevation: 0.0,
-        backgroundColor: Colors.amberAccent,
         title: Text(
           'New Card',
-          style: GoogleFonts.lato(
-            fontSize: 25.0,
-            color: Colors.black,
-          ),
         ),
       ),
       body: SafeArea(
         bottom: false,
-        child: DirectSelectContainer(
-          child: Container(
-            padding: const EdgeInsets.all(30.0),
-            color: Colors.amberAccent,
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.only(bottom: 30.0),
-                  child: InputComponent(
-                    controller: this._emailController,
-                    label: 'Title',
-                    icon: Icons.text_fields,
-                  ),
+        child: Container(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(bottom: 30.0),
+                child: InputComponent(
+                  controller: this._titleController,
+                  label: 'Title',
+                  icon: Icons.text_fields,
                 ),
-                ButtonComponent(
-                  onPressed: () {},
-                  screenWidth: size.width,
-                  label: 'Save',
-                ),
-              ],
-            ),
+              ),
+              ButtonComponent(
+                onPressed: () {},
+                screenWidth: size.width,
+                label: 'Save',
+              ),
+            ],
           ),
         ),
       ),
