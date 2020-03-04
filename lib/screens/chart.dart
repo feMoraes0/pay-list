9407b77ac6f48499d1593491ca50c4034fc41626
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-03-04 16:25:20 +0100

    refactor<style>: chart style

--- lib/screens/chart.dart
+++ lib/screens/chart.dart
@@ -8,63 +8,62 @@ class LineChartSample2 extends StatefulWidget {
 
 class _LineChartSample2State extends State<LineChartSample2> {
   List<Color> gradientColors = [
-    Colors.deepOrange,
-    Colors.deepOrange,
+    Colors.deepOrangeAccent,
+    Colors.deepOrangeAccent,
   ];
-
-  bool showAvg = false;
 
   @override
   Widget build(BuildContext context) {
-    return LineChart(mainData());
-  }
-
-  LineChartData mainData() {
-    return LineChartData(
-      gridData: FlGridData(
-        show: false,
-      ),
-      titlesData: FlTitlesData(
-        show: true,
-        bottomTitles: SideTitles(
-          showTitles: false,
+    return LineChart(
+      LineChartData(
+        gridData: FlGridData(
+          show: false,
         ),
-        leftTitles: SideTitles(
-          showTitles: false,
-        ),
-      ),
-      borderData: FlBorderData(
-        show: false,
-      ),
-      minX: 0,
-      maxX: 11,
-      minY: 0,
-      maxY: 6,
-      lineBarsData: [
-        LineChartBarData(
-          spots: const [
-            FlSpot(0, 3),
-            FlSpot(2.6, 2),
-            FlSpot(4.9, 5),
-            FlSpot(6.8, 3.1),
-            FlSpot(8, 4),
-            FlSpot(9.5, 3),
-            FlSpot(11, 4),
-          ],
-          isCurved: true,
-          colors: gradientColors,
-          barWidth: 5,
-          isStrokeCapRound: true,
-          dotData: const FlDotData(
-            show: false,
+        titlesData: FlTitlesData(
+          show: true,
+          bottomTitles: SideTitles(
+            showTitles: false,
           ),
-          belowBarData: BarAreaData(
-            show: true,
-            colors:
-                gradientColors.map((color) => color.withOpacity(0.4)).toList(),
+          leftTitles: SideTitles(
+            showTitles: false,
           ),
         ),
-      ],
+        borderData: FlBorderData(
+          show: false,
+        ),
+        minX: 0,
+        maxX: 11,
+        minY: 0,
+        maxY: 6,
+        lineBarsData: [
+          LineChartBarData(
+            spots: [
+              FlSpot(0, 3),
+              FlSpot(2.6, 2),
+              FlSpot(4.9, 5),
+              FlSpot(6.8, 3.1),
+              FlSpot(8, 4),
+              FlSpot(9.5, 3),
+              FlSpot(11, 4),
+            ],
+            isCurved: true,
+            colors: gradientColors,
+            barWidth: 5,
+            isStrokeCapRound: true,
+            dotData: const FlDotData(
+              show: false,
+            ),
+            belowBarData: BarAreaData(
+              show: true,
+              colors: gradientColors
+                  .map(
+                    (color) => color.withOpacity(0.5),
+                  )
+                  .toList(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
